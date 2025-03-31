9da1879df543e380eab0ef414b23ae0cb73bd99b
mklepac90 <mklepac90@gmail.com>
2025-03-31 13:47:52 -0400

    clean up reporting

--- main.go
+++ main.go
@@ -49,7 +49,5 @@ func main() {
 	go cfg.crawlPage(rawBaseURL)
 	cfg.wg.Wait()
 
-	for normalizedURL, count := range cfg.pages {
-		fmt.Printf("%d - %s\n", count, normalizedURL)	
-	}
+	printReport(cfg.pages, rawBaseURL)
 }
