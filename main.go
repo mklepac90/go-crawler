48d93d86308ea63c5fa05d66b2f7f403484cf8f8
mklepac90 <mklepac90@gmail.com>
2025-03-31 13:40:00 -0400

    add more config vars

--- main.go
+++ main.go
@@ -3,25 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	args := os.Args[1:]
 	
-	if len(args) < 1 {
-		fmt.Println("no website provided")
+	if len(args) < 3 {
+		fmt.Println("not enough arguments provided")
+		fmt.Println("usage: crawler <baseURL> <maxConcurrency> <maxPages>")
 		os.Exit(1)
 	}
 
-	 if len(args) > 1 {
+	 if len(args) > 3 {
 		fmt.Println("too many arguments provided")
+		fmt.Println("usage: crawler <baseURL> <maxConcurrency> <maxPages>")
 		os.Exit(1)
 	}
 
 	rawBaseURL := args[0]
+	maxConcurrencyString := args[1]
+	maxPagesString := args[2]
+
+	maxConcurrency, err := strconv.Atoi(maxConcurrencyString)
+	if err != nil {
+		fmt.Printf("Error - maxConcurrency: %v", err)
+		return
+	}
+
+	maxPages, err := strconv.Atoi(maxPagesString)
+	if err != nil {
+		fmt.Printf("Error - maxPages: %v", err)
+		return
+	}
 	
-	const maxConcurrency = 3
-	cfg, err := configure(rawBaseURL, maxConcurrency)
+	cfg, err := configure(rawBaseURL, maxConcurrency, maxPages)
 	if err != nil {
 		fmt.Printf("Error - configure: %v", err)
 		return
