63c85500a096e943c97690ab5b809da1faeedbfd
mklepac90 <mklepac90@gmail.com>
2025-04-16 14:08:46 -0400

    write to csv

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # go-crawler
+
+1. error handling & retries + backoff whatever
+2. check robots
+3. count external links & add to report
+4. save report as csv (email it as well?)
+5. set up a chron for the above
+6. follow external links???
--- print_report.go
+++ print_report.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"sort"
+	"os"
+	"encoding/csv"
+	"strconv"
+	"time"
 )
 
 type Page struct {
@@ -18,23 +22,41 @@ func printReport(pages map[string]int, baseURL string) {
 `, baseURL)
 
 	sortedPages := sortPages(pages)
+	
+	now := time.Now()
+	fileName := fmt.Sprintf("report_%v.csv", now.Unix())
+	file, err := os.Create(fileName)
+	defer file.Close()
+        if err != nil {
+                fmt.Printf("failed creating file: %v", err)
+        }
+
+        writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"url", "count"})
 	for _, page := range sortedPages {
 		url := page.URL
 		count := page.Count
 		fmt.Printf("Found %d internal links to %s\n", count, url)
+		writer.Write([]string{url, strconv.Itoa(count)})
 	}
 }
 
 func sortPages(pages map[string]int) []Page {
 	pagesSlice := []Page{}
+
 	for url, count := range pages {
 		pagesSlice = append(pagesSlice, Page{URL: url, Count: count})
 	}
+
 	sort.Slice(pagesSlice, func(i, j int) bool {
 		if pagesSlice[i].Count == pagesSlice[j].Count {
 			return pagesSlice[i].URL < pagesSlice[j].URL
 		}
+
 		return pagesSlice[i].Count > pagesSlice[j].Count
 	})
+
 	return pagesSlice
 }
